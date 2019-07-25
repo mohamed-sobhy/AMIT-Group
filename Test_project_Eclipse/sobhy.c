6279ef0ee39da96c222e21206636b9a0be4ecc6d
Mohamed Sobhy <Mohamed Sobhy@Sobhy>
2019-07-25 23:50:20 +0200

    solve merge message in the file

--- Test_project_Eclipse/sobhy.c
+++ Test_project_Eclipse/sobhy.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 void main(void) {
 
-<<<<<<< HEAD
-=======
 
->>>>>>> refs/remotes/origin/master
 int x=333 ;
-<<<<<<< HEAD
-=======
 
-
->>>>>>> refs/remotes/origin/master
 
 }
