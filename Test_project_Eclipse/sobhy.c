be0cd4c779909829ae0a75d1442ac84088070811
Mohamed Sobhy <Mohamed Sobhy@Sobhy>
2019-07-25 23:29:20 +0200

    x = 333

--- Test_project_Eclipse/sobhy.c
+++ Test_project_Eclipse/sobhy.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 void main(void) {
 
-<<<<<<< HEAD
+
 int x=333 ;
-int y=5;
-=======
-int x = 20 ;
->>>>>>> branch 'master' of https://github.com/mohamed-sobhy/AMIT-Group
+
 
 
 }
