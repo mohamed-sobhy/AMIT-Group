681a55faaaf747c867e53e6f5254c8013ac8212e
Mohamed Sobhy <m.sobhy.awad@gmail.com>
2019-07-26 08:00:33 +0200

    Revert "handle sobhy"
    This reverts commit 6980a07234f0f807b39dec41a64dd2ebeaa494ef.

--- Test_project_Eclipse/sobhy.c
+++ Test_project_Eclipse/sobhy.c
@@ -4,6 +4,17 @@ void main(void) {
 
 int x=333 ;
 int c=10;
-
+<<<<<<< HEAD
+c5tgtmmmmmmmmmmmmmmmm
+jjjjjjjjjjjjjjjjjj
+=======
+<<<<<<< Upstream, based on origin/feature2
+c5tgt
+jjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkknnnnnnnnnn
+=======
+c5tgtnnnnnnn
+jjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkk
+>>>>>>> ee92c79 nnnabove
+>>>>>>> branch 'feature2' of https://github.com/mohamed-sobhy/AMIT-Group
 }
 
